8865013cc9ec8d07d698b01feec0c0535a26c2ea
vla5924 <vla2000@inbox.ru>
2020-07-23 12:07:43 +0300

    docs for renderer

--- core/include/renderer.h
+++ core/include/renderer.h
@@ -6,15 +6,36 @@
 
 #pragma comment (lib, "opengl32.lib")
 
+/**
+ * @brief Rendering controller
+ */
 class Renderer : public QOpenGLWidget
 {
 private:
     QOpenGLFunctions* open_gl_functions;
 
 public:
+    /**
+     * @brief Default constructor
+     * @param parent Parent widget if needed
+     */
     Renderer(QWidget* parent = nullptr);
 
+
+    /**
+     * @brief Initializes GL context
+     */
     void initializeGL();
+
+    /**
+     * @brief Resizes GL picture
+     * @param width Frame width
+     * @param height Frame height
+     */
     void resizeGL(int width, int height);
+
+    /**
+     * @brief Paints GL picture
+     */
     void paintGL();
 };
