8a4bb3ebe9375b4e31098b5f59a593b129c3b10d
Mikhail-Zoreev <mzoreev@gmail.com>
2020-08-04 12:15:52 +0300

    add exception throwing

--- core/src/graphics/texture.cpp
+++ core/src/graphics/texture.cpp
@@ -8,6 +8,7 @@ namespace Graphics
         if (surface == nullptr)
         {
             SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Can't load file %s", path.c_str());
+            throw std::exception("Can't load file");
         }
         size = { surface->h, surface->w };
         sdl_texture = SDL_CreateTextureFromSurface(renderer, surface);
