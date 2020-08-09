1252ba35c194e219fe38aba2ca946c212351380a
vla5924 <vla2000@inbox.ru>
2020-08-09 21:43:22 +0300

    Font documentation

--- core/include/graphics/font.h
+++ core/include/graphics/font.h
@@ -10,27 +10,87 @@
 
 namespace Graphics
 {
+    /**
+     * @brief Font renderer
+     * @details Class for rendering and calculating metrics of TrueType font
+     */
     class Font
     {
     protected:
+        /// True if SDL-ttf library is initialized
         static bool initialized;
+
+        /// Font data pointer
         TTF_Font* p_font;
+
+        /// Path to .ttf file
         const std::string c_path;
+
+        /// Average char width (calculated by calculateAverageCharWidth() method)
         float m_average_char_width;
 
+        /**
+         * @brief Calculates average char width
+         * @details This method is called in constructor. It saves calculated
+         * value in m_average_char_width variable.
+         * @note Width is calculated as an average width of symbol in string
+         * "jiwmhx ote" (average width = total width / 10)
+         */
         void calculateAverageCharWidth();
 
+
     public:
+        /// Default constructor is not provided
         Font() = delete;
-        explicit Font(const std::string& ttf_path, int font_size);
+
+        /**
+         * @brief Constructor-initializer
+         * @param ttf_path Path to .ttf file with TrueType font
+         * @param font_size Size of glyphs (in pixels) to load
+         */
+        Font(const std::string& ttf_path, int font_size);
+
+        /// Destructor
         ~Font();
 
+
+        /**
+         * @brief Getter for string with path to loaded font file
+         * @return std::string Path to font file
+         */
         std::string path() const;
+
+        /**
+         * @brief Getter for average character width
+         * @details See calculateAverageCharWidth()
+         * @return float Average character width
+         */
         float averageCharWidth() const;
 
+        /**
+         * @brief Calculates potential width of surface with given string
+         * @details (No rendering)
+         * @param text String to calculate width of
+         * @return unsigned Width of surface with text
+         */
         unsigned calculateTextWidth(const std::string& text);
+
+        /**
+         * @brief Calculates potential size of surface with given string
+         * @details (No rendering)
+         * @param text String to calculate size of
+         * @return SDL_Rect Rectangle with size of surface with text
+         */
         SDL_Rect calculateTextRect(const std::string& text);
 
+
+        /**
+         * @brief Renders given text string as texture image
+         * @param renderer Renderer
+         * @param text Text string (UTF-8) to render
+         * @param color Font color
+         * @return std::shared_ptr<Graphics::Texture> Pointer to rendered texture
+         */
         std::shared_ptr<Graphics::Texture> renderText(SDL_Renderer* renderer, const std::string& text, SDL_Color color);
     };
 }
