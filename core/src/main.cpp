68c56176a6f0953efa7e9fcc78104a8e082a907f
vla5924 <vla2000@inbox.ru>
2020-08-09 21:18:16 +0300

    database test (in core)

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -2,8 +2,39 @@
 
 #include "window.h"
 
+#include <database.h>
+
 int main(int argc, char *argv[])
 {
+	try
+	{
+		Database::Connection db("sqlite.db");
+		Database::Statement query(db,
+			"CREATE TABLE IF NOT EXISTS `test` (" \
+			"`ID` INT PRIMARY KEY NOT NULL," \
+			"`NAME` TEXT NOT NULL," \
+			"`AGE` INT NOT NULL" \
+			");"
+		);
+		query.fetchStep();
+		Database::Statement query2(db,
+			"INSERT INTO `test` (`ID`, `NAME`, `AGE`) VALUES (1, \"Vasya1\", 123);"
+		);
+		query2.fetchStep();
+		Database::Statement query3(db,
+			"SELECT * FROM `test`;"
+		);
+		while (query3.fetchStep())
+		{
+			std::cout << query3.columnAs<int>(0) << query3.columnAs<std::string>(1) << query3.columnAs<int>(2);
+		}
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what();
+	}
+
+
 	Window window;
 	window.show();
 	window.start();
