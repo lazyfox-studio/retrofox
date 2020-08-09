33ddcd2b7dcae96ec38f6385436201673417bbb5
vla5924 <vla2000@inbox.ru>
2020-08-09 22:37:21 +0300

    Layout documentation

--- core/include/interface/layout.h
+++ core/include/interface/layout.h
@@ -9,48 +9,85 @@ namespace Interface
     using WidgetRefList = std::list<std::reference_wrapper<Interface::Widget>>;
 
     /**
-     * @brief Contains widgets
+     * @brief Layout widget
+     * @details Class to represent multiple widgets arranged in a horizontal 
+     * or vertical stack called 'layout' (with automated aligning and padding)
     */
     class Layout : public Widget
     {
     protected:
+        /// List of references to widgets
         WidgetRefList widgets;
 
     public:
+        /// Represents margin size
         struct Margin
         {
-            int left;
-            int top;   
-            int right;
-            int bottom;
+            int left;   ///< Left margin
+            int top;    ///< Top margin
+            int right;  ///< Right margin
+            int bottom; ///< Bottom margin
         };
-        Margin margin;
+        Margin margin; ///< Widgets margin
 
+        /// Represents spacing size
         struct Spacing
         {
-            int vertical;
-            int horizontal;
+            int vertical;   ///< Vertical spacing
+            int horizontal; ///< Horizontal spacing
         };
-        Spacing spacing;
+        Spacing spacing; ///< Widgets spacing
 
+        /// Represents type of stacking
         enum class Stacking
         {
-            Vertical,
-            Horizontal
+            Vertical,  ///< Vertical stacking
+            Horizontal ///< Horizontal stacking
         };
-        Stacking stacking;
+        Stacking stacking; ///< Widgets stacking type
 
+        /// Default constructor
         Layout();
+
+        /**
+         * @brief Constructor-initializer
+         * @param x Coordinate X value
+         * @param y Coordinate Y value
+         * @param height Height value
+         * @param width Width value
+         */
         Layout(int x, int y, unsigned height, unsigned width);
+
+        /// Destructor
         ~Layout();
 
+
+        /**
+         * @brief Adds widget to front of list
+         * @param widget Widget to add
+         */
         void pushFront(Widget& widget);
+
+        /**
+         * @brief Adds widget to back of list
+         * @param widget Widget to add
+         */
         void pushBack(Widget& widget);
+
+        /// Removes first widget from list
         void popFront();
+
+        /// Removes last widget from list
         void popBack();
+
+        /// Removes all widgets from list
         void clear();
+
+
+        /// Renders all widgets in list
         virtual void render();
 
+        /// Recalculates widget parameters
         void update();
     };
 }
