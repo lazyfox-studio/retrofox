f71bdf4aa50ddcdbee6e2a371714ac9c93ffed5f
vla5924 <vla2000@inbox.ru>
2020-08-10 16:27:26 +0300

    column fetcher usage in gamemenu

--- core/src/gamemenu.cpp
+++ core/src/gamemenu.cpp
@@ -16,13 +16,12 @@ void GameMenu::showGames(SDL_Renderer* renderer, std::string base_path)
     Database::Connection base(base_path);
     layout.clear();
     widgets.clear();
-    Database::Statement query = base.query("SELECT name FROM 'games';");
-    Database::Row row;
-    
-    while (row = query.fetchRow())
+
+    auto games = base.getColumn<std::string>("SELECT `name` FROM `games`;");
+    for (const auto& name : games)
     {
         Interface::LabeledButton* button = new Interface::LabeledButton //TODO: REMOVE MEMORY LEAK!!!!!!!!!!!!!!!!!!!
-            (renderer, row.column<std::string>(0), "../../core/data/testsprite.bmp", "../../core/data/testsprite2.bmp");
+        (renderer, name, "../../core/data/testsprite.bmp", "../../core/data/testsprite2.bmp");
         pushBack(*button);
     }
 }
