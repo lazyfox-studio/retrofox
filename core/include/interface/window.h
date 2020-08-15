6291c48ac55c873fe810e510ad1a250d41ea5700
vla5924 <vla2000@inbox.ru>
2020-08-15 13:44:07 +0300

    Window documentation

--- core/include/interface/window.h
+++ core/include/interface/window.h
@@ -10,31 +10,51 @@
 
 namespace Interface
 {
+    /**
+     * @brief System window
+     * @details Class to represent window
+     */
     class Window
     {
     protected:
+        /// Pointer to window data
         SDL_Window* window;
-        SDL_Renderer* renderer;
 
+        /// Pointer to renderer
+        SDL_Renderer* renderer;
+        
+        /// Pointer to root widget
         std::shared_ptr<Interface::Widget> root_widget;
 
     public:
+        /// Default constructor is not provided
         Window() = delete;
+
+        /**
+         * @brief Constructor-initializer
+         * @details Creates window and renderer as SDL objects
+         * @param title Window title
+         * @param x Coordinate X value
+         * @param y Coordinate Y value
+         * @param width Width value
+         * @param height Height value
+         */
         Window(const std::string& title, int x, int y, int width, int height);
+
+        /// Destructor
         ~Window();
 
+        
+        /// Initializes window
         virtual void initialize();
 
+        /// Gets window into full-screen mode
         void setFullscreen();
 
-        /**
-         * @brief Show window
-        */
+        /// Lets window display
         void show();
-        /**
-         * @brief Start application loop
-        */
-        void start();
 
+        /// Starts application loop
+        void start();
     };
 }
