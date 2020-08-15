f8c04b1bf6e1e4432d3ce808fcba8194e0183ee6
vla5924 <vla2000@inbox.ru>
2020-08-15 13:46:12 +0300

    Main window documentation

--- core/include/mainwindow.h
+++ core/include/mainwindow.h
@@ -6,12 +6,29 @@
 
 #include "gamemenu.h"
 
+/**
+ * @brief Main window of application
+ * @details Class to represent main window of application
+ */
 class MainWindow : public Interface::Window
 {
 public:
+    /// Default constructor is not provided
     MainWindow() = delete;
+
+    /**
+     * @brief Constructor-initializer
+     * @param title Window title
+     * @param x Coordinate X value
+     * @param y Coordinate Y value
+     * @param width Width value
+     * @param height Height value
+     */
     MainWindow(const std::string& title, int x, int y, int width, int height);
+
+    /// Destructor
     ~MainWindow();
 
+    /// Initializes window
     virtual void initialize();
 };
