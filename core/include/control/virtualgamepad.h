dea8ff1239ec81b2be71df935cc8867067842642
vla5924 <vla2000@inbox.ru>
2020-08-15 13:22:53 +0300

    Virtual gamepad documentation

--- core/include/control/virtualgamepad.h
+++ core/include/control/virtualgamepad.h
@@ -4,30 +4,38 @@
 
 namespace Control
 {
+    /**
+     * @brief Virtual gamepad controller
+     * @details Class for processing events from gamepad controls
+     */
     class VirtualGamepad
     {
     public:
+        /// Available buttons to control
         enum KeyCode
         {
-            Empty,
-            /// Left cross
-            Up,
-            Down,
-            Left,
-            Right,
-            /// Right buttons bar
-            A,
-            B,
-            X,
-            Y
+            Empty, ///< Empty (unknown button pressed)
+            Up,    ///< Up (left cross)
+            Down,  ///< Down (left cross)
+            Left,  ///< Left (left cross)
+            Right, ///< Right (left cross)
+            A,     ///< A button (right bar)
+            B,     ///< B button (right bar)
+            X,     ///< X button (right bar)
+            Y      ///< Y button (right bar)
         };
 
         /**
-         * @brief Process input, and update virtual gamepad status
-         * @param event Event that may be an input event
-        */
+         * @brief Processes input and updates virtual gamepad status
+         * @param event Event to process (may be an input event)
+         * @return Button key code
+         */
         KeyCode processInput(SDL_Event event);
 
+        /**
+         * @brief Returns virtual gamepad controller instance
+         * @return Instance reference
+         */
         static VirtualGamepad& instance();
     };
 }
