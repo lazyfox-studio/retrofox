b28f5fb1b365525711d44bd87ee15f9b16091475
vla5924 <vla2000@inbox.ru>
2020-08-15 14:59:25 +0300

    Statement documatetaion

--- database/include/statement.h
+++ database/include/statement.h
@@ -10,44 +10,102 @@
 
 namespace Database
 {
+    /**
+     * @brief SQLite3 statement
+     * @details Class to represent prepared statement
+     */
     class Statement
     {
     protected:
         friend class Connection;
 
+        /// Statement handler
         sqlite3_stmt* p_stmt_handler;
 
+        /**
+         * @brief Internal constructor-initializer
+         * @param stmt_handler Statement handler
+         */
         explicit Statement(sqlite3_stmt* p_stmt_handler);
 
     public:
+        /// Default constructor
         Statement();
+
+        /// Destructor
         ~Statement();
 
+        /// Move constructor
         Statement(Statement&& other);
+
+        /// Move assignment operator
         Statement& operator=(Statement&& other);
 
+        /// Copy constructor is not provided
         Statement(const Statement&) = delete;
+
+        /// Copy assignment operator is not provided
         Statement& operator=(const Statement&) = delete;
 
+
+        /// Executes query (fetches rows as many times as possible)
         void execute();
+
+        /**
+         * @brief Fetches sequential row (executes single step) 
+         * @return Row
+         */
         Row fetchRow();
 
+
+        /**
+         * @brief Returns single column values as vector
+         * @details Helper method: fetches first columns of all rows into vector
+         * @return Vector with column values
+         */
         template<typename Value>
         std::vector<Value> getColumn();
 
+        /**
+         * @brief Returns two columns values as map
+         * @details Helper method: fetches first two columns of all rows (as key-value) into map
+         * @return Map with columns values
+         */
         template<typename Key, typename Value>
         std::map<Key, Value> getIndexedColumn();
         
+
+        /**
+         * @brief Returns number of columns
+         * @return Number of columns
+         */
         int columnCount() const;
+
+        /**
+         * @brief Returns size of column data
+         * @param column_index Column index
+         * @return Number of bytes
+         */
         int columnBytes(int column_index) const;
 
+        /**
+         * @brief Resets statement so it could be preformed anew
+         * @param clear_bindings If true, also clears binded values
+         */
         void reset(bool clear_bindings = true);
 
+        /**
+         * @brief Binds SQL NULL value to placeholder
+         * @param placeholder_index Placeholder index
+         */
         void bindNull(int placeholder_index);
 
+        /**
+         * @brief Binds value in preferred type (Type) to placeholder
+         * @param placeholder_index Placeholder index
+         */
         template<typename Type>
         void bind(int placeholder_index, Type binding_value);
-
     };
 
 
