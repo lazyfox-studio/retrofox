e943aedf284540d3cc2858a219997477492fd24a
vla5924 <vla2000@inbox.ru>
2020-08-15 15:05:54 +0300

    entities documentation

--- database/include/entities/emulator.h
+++ database/include/entities/emulator.h
@@ -10,15 +10,25 @@ namespace Database
 {
     namespace Entities
     {
+        /// Emulator entity
         struct Emulator
         {
-            unsigned id;
-            std::string name;
-            unsigned platform_id;
-            std::string execution_command;
+            unsigned id; ///< Emulator ID (ROWID)
+            std::string name; ///< Emulator name
+            unsigned platform_id; ///< Emulator platform ID
+            std::string execution_command; /// Execution command
 
+            /**
+             * @brief Typecast constructor
+             * @param row Row with emulator info
+             */
             Emulator(const Database::Row& row);
 
+            /**
+             * @brief Fetches all rows and extracts entities info from them into vector
+             * @param stmt Statement with SELECT query
+             * @return Vector of entities
+             */
             static std::vector<Emulator> fetchEntities(Database::Statement& stmt);
         };
     }
