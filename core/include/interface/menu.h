87a241585b39272d81d45438daef1c5149f40c2f
vla5924 <vla2000@inbox.ru>
2020-08-15 14:05:26 +0300

    menu documentation

--- core/include/interface/menu.h
+++ core/include/interface/menu.h
@@ -7,42 +7,119 @@
 
 namespace Interface
 {
+    using ButtonRefList = std::list<std::reference_wrapper<Interface::Button>>;
+
+    /**
+     * @brief Menu with buttons
+     * @details Class to represent menu with buttons list
+     */
     class Menu : public Widget
     {
     protected:
+        /// Layout with buttons
         Interface::Layout layout;
-        std::list<std::reference_wrapper<Interface::Button>> widgets;
 
-        std::list<std::reference_wrapper<Interface::Button>>::iterator current;
-        std::list<std::reference_wrapper<Interface::Button>>::iterator first;
-        std::list<std::reference_wrapper<Interface::Button>>::iterator last;
+        /// List of buttons
+        ButtonRefList widgets;
 
+        ButtonRefList::iterator current; ///< Current button iterator
+        ButtonRefList::iterator first;   ///< First button iterator
+        ButtonRefList::iterator last;    ///< Last button iterator
+
+        /// Height of each button
         unsigned m_button_height;
+
+        /// Number of visible buttons at the moment
         size_t visible_buttons_count;
 
     public:
+        /// Default constructor
         Menu();
+
+        /**
+         * @brief Constructor-initializer
+         * @param x Coordinate X value
+         * @param y Coordinate Y value
+         * @param height Height value
+         * @param width Width value
+         * @param button_height Height of each button in menu
+         */
         Menu(int x, int y, unsigned height, unsigned width, unsigned button_height);
+
+        /// Destructor
         ~Menu();
 
+
+        /// Resets internal iterators
         void reset();
 
-        void pushFront(Button& widget);
-        void pushBack(Button& widget);
+        /**
+         * @brief Adds button to front of list
+         * @param button Button to add
+         */
+        void pushFront(Button& button);
 
+        /**
+         * @brief Adds button to back of list
+         * @param button Button to add
+         */
+        void pushBack(Button& button);
+
+
+        /**
+         * @brief Switches to next button
+         * @return True on success
+         */
         bool next();
+
+        /**
+         * @brief Switches to previous button
+         * @return True on success
+         */
         bool previous();
 
+
+        /// Makes widget active
         virtual void setActive();
+
+        /// Makes widget inactive (ignore control events)
         virtual void setInactive();
 
+
+        /**
+         * @brief Setter for coordinate X value
+         * @param x New coordinate X value
+         */
         virtual void setX(int x);
+
+        /**
+         * @brief Setter for coordinate Y value
+         * @param y New coordinate Y value
+         */
         virtual void setY(int y);
+
+        /**
+         * @brief Setter for height value
+         * @param height New height value
+         */
         virtual void setHeight(unsigned height);
+
+        /**
+         * @brief Setter for width value
+         * @param width New width value
+         */
         virtual void setWidth(unsigned width);
 
+
+        /**
+         * @brief Processes control event from virtual gamepad
+         * @param code Button key code
+         * @return True if processed by this widget
+         */
         virtual bool onControl(Control::VirtualGamepad::KeyCode code);
 
+
+        /// Renders menu via provided renderer
         virtual void render();
     };
 }
