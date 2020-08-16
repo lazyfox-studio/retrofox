52dd83c25b8e06d33ed81f2a62899815e83f3696
Mikhail-Zoreev <mzoreev@gmail.com>
2020-08-16 17:28:12 +0300

    game name output

--- api-service/scripts/igdb.py
+++ api-service/scripts/igdb.py
@@ -1,7 +1,12 @@
 import requests
+import json
 
 headers = {"user-key": "58446688c5995387afb5a340e54ee0a5"}
-data = "fields *; search \"Clock Tower\"; where platforms = (58);"
+data = "fields *; search \"Clock Tower\"; where platforms = (7);"
 
 answer = requests.post("https://api-v3.igdb.com/games", headers=headers, data=data)
-print(answer.content)
+json_string = answer.content
+
+games = json.loads(json_string)
+for game in games:
+    print(game["name"])
