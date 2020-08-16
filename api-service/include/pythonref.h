55b528ea21bf9543544bec1df7e37d5a128e5d68
vla5924 <vla2000@inbox.ru>
2020-08-16 19:43:25 +0300

    PythonRef documentation

--- api-service/include/pythonref.h
+++ api-service/include/pythonref.h
@@ -2,23 +2,48 @@
 
 #include <Python.h>
 
+/**
+ * @brief Simple wrapper for py-reference
+ * @details Class to represent py-reference and its typecasting
+ */
 class PythonRef
 {
 protected:
     friend class PythonEnv;
 
+    /// Pointer to py-reference
     PyObject* p_ref;
 
+    /**
+     * @brief Typecast constructor
+     * @param ref Pointer to py-reference to hold
+     */
     PythonRef(PyObject* ref);
+
 public:
+    /// Default constructor is not provided
     PythonRef() = delete;
+
+    /// Copy constructor is not provider
     PythonRef(const PythonRef&) = delete;
+
+    /// Move constructor
     PythonRef(PythonRef&& other);
+
+    /// Destructor
     ~PythonRef();
 
+    /// Copy assignment operator is not provided
     PythonRef& operator=(const PythonRef&) = delete;
+
+    /// Move assignment operator
     PythonRef& operator=(PythonRef&& other);
 
+    /**
+     * @brief Casts object (in py-reference) to preferred type (RType)
+     * @details Available types: int, long
+     * @return Value with RType type
+     */
     template<typename RType>
     RType cast();
 };
