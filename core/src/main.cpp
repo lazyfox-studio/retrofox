e0d7a022ca972f4698cb0ae9847a50502d4ce7f0
vla5924 <vla2000@inbox.ru>
2020-08-16 18:11:50 +0300

    PythonEnv test (still doesn't work)

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <database.h>
+#include <pythonenv.h>
 
 #include "mainwindow.h"
 
@@ -8,31 +9,13 @@ int main(int argc, char *argv[])
 {
 	try
 	{
-		Database::Connection db("sqlite.db");
-		Database::Statement query;
-		query = db.query(
-			"CREATE TABLE IF NOT EXISTS `test` (" \
-			"`ID` INT PRIMARY KEY NOT NULL," \
-			"`NAME` TEXT NOT NULL," \
-			"`AGE` INT NOT NULL" \
-			");"
-		);
-		query.execute();
-		//query = db.query("INSERT INTO `test` (`ID`, `NAME`, `AGE`) VALUES (1, \"Vasya1\", 123);");
-		//query.execute();
-		query = db.query("SELECT * FROM `test` WHERE `ID` = ?;");
-		query.bind(1, "1");
-		Database::Row row;
-		while (row = query.fetchRow())
-		{
-			std::cout << row.column<int>("ID") << row.column<std::string>("NAME") << row.column<int>(2);
-		}
+		PythonEnv::instance().loadModule("test.py");
+		int result = PythonEnv::instance().callFunction("test.py", "someMagicFunctionWithoutArgs").cast<int>();
 	}
 	catch (std::exception & e)
 	{
 		std::cerr << e.what();
 	}
-
 
 	MainWindow window("RetroFox", 100, 100, 800, 600);
 	//window.setFullscreen();
