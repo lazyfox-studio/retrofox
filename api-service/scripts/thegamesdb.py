f6af4c7d247b8c7c549f1255751b982fc547effe
Mikhail-Zoreev <mzoreev@gmail.com>
2020-08-17 11:14:19 +0300

    add platform filtering

--- api-service/scripts/thegamesdb.py
+++ api-service/scripts/thegamesdb.py
@@ -5,30 +5,29 @@ import requests
 
 
 def find_game(api_key, path, platform):
-    #Preparing query
+    # Preparing query
     query_string = os.path.splitext(path)[0]
     query_string = os.path.basename(query_string)
     query_string = re.sub(r'\[.*\]', '', query_string)
     query_string = re.sub(r'\(.*\)', '', query_string)
-    if query_string.startswith(" "):
-        query_string = query_string[1:]
+    query_string = query_string.strip(" ")
     print(query_string)
 
-    #Sending request
-    queary_params = {
+    # Sending request
+    query_params = {
         'apikey': api_key,
         'name': query_string,
-        'platfrom': platform
+        'filter[platform]': platform
     }
-    answer = requests.get("https://api.thegamesdb.net/v1.1/Games/ByGameName", params=queary_params)
+    answer = requests.get("https://api.thegamesdb.net/v1.1/Games/ByGameName", params=query_params)
     games = json.loads(answer.content)['data']
 
-    #Remove invaild games
+    # Remove invalid games
     delete_list = []
 
-    #Chck if seraching for first game in series
+    # Check if searching for first game in series
     if not games['count'] == 1:
-        if (games['count'] == 0):
+        if games['count'] == 0:
             return 1
         if not re.search(r'\d', query_string):
             for game in games['games']:
@@ -39,9 +38,9 @@ def find_game(api_key, path, platform):
         games['games'].remove(game)
     delete_list = []
 
-    #Remove all "xxx edition" from result
+    # Remove all "xxx edition" from result
     if not games['count'] == 1:
-        if (games['count'] == 0):
+        if games['count'] == 0:
             return 1
         for game in games['games']:
             if re.search(r'\(.*\)', game['game_title']):
@@ -54,7 +53,7 @@ def find_game(api_key, path, platform):
     games = games['games']
 
     print(games)
-    #Add to database
+    # Add to database
 
 
 find_game('445fcbc3f32bb2474bc27016b99eb963d318ee3a608212c543b9a79de1041600', 'D:/Games/(1996) Resident Evil[NTSC].cue', '10')
