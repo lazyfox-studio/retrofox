26041e43bf92d52d8809bb70bd064b837326f8ae
Mikhail-Zoreev <mzoreev@gmail.com>
2020-08-24 14:59:48 +0300

    python fixes

--- api-service/scripts/findgames.py
+++ api-service/scripts/findgames.py
@@ -12,13 +12,14 @@ def scan_folder(path, extensions):
         for extension in extensions:
             if fs_object.endswith(extension):
                 roms.append(path + '/' + fs_object)
+            break
     return roms
 
 
 def find_games_in_folder(roms_path, platform_id, path_to_db):
     base = sqlite3.connect(path_to_db)
     cursor = base.cursor()
-    cursor.execute('SELECT extension FROM extensions WHERE platform_id=?', (platform_id))
+    cursor.execute('SELECT extension FROM extensions WHERE platform_id=?', (platform_id, ))
     extensions = cursor.fetchall()
     roms = scan_folder(roms_path, extensions)
     for rom in roms:
@@ -33,7 +34,7 @@ def find_game(api_key, game_id, path_to_db):
 
     base = sqlite3.connect(path_to_db)
     cursor = base.cursor()
-    cursor.execute('SELECT path, platform_id FROM games WHERE id = game_id')
+    cursor.execute('SELECT path, platform_id FROM games WHERE id = ?', (game_id, ))
     game_information = cursor.fetchone()
     path_to_game = game_information[0]
     platform = game_information[1]
@@ -41,7 +42,7 @@ def find_game(api_key, game_id, path_to_db):
 
     query_string = service.prepare_query_string(path_to_game)
     raw_games_data = service.load_raw_games_data(api_key, query_string, platform)
-    if raw_games_data == None:
+    if raw_games_data is None:
         return 1  # Request error
     games = service.extract_games_data(raw_games_data, game_id, query_string, '../../sln/core/testbase.db')
     games = GameFilter.remove_sequels(games, query_string)
@@ -55,7 +56,7 @@ def find_game(api_key, game_id, path_to_db):
         return 3  # Database error
 
 
-thegamesdbapiservice.BaseAPIService.set_up_tables('E:/Source/retrofox/sln/core/testbase.db')
+#thegamesdbapiservice.BaseAPIService.set_up_tables('E:/Source/retrofox/sln/core/testbase.db')
 #find_games_in_folder('Z:\PlayStation', 10, 'E:/Source/retrofox/sln/core/testbase.db')
 #find_game('445fcbc3f32bb2474bc27016b99eb963d318ee3a608212c543b9a79de1041600', 1, 'E:/Source/retrofox/sln/core/testbase.db')
 
