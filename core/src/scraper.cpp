a6c415912fde7c7bdc24277f069c58a2531ebbdb
vla5924 <vla2000@inbox.ru>
2020-08-25 14:36:38 +0300

    new helper usage in scraper
    + small codestyle fixes

--- core/src/scraper.cpp
+++ core/src/scraper.cpp
@@ -12,39 +12,33 @@ namespace Scraper
         Database::Connection base(db_path);
         auto games = base.getColumn<std::string>("SELECT `id` FROM `games`;");
 
-        //TODO: ADD KEY GET FROM SERVER
+        // TODO: ADD KEY GET FROM SERVER
         std::string stolen_api_key = "445fcbc3f32bb2474bc27016b99eb963d318ee3a608212c543b9a79de1041600";
 
         for (const auto& id : games)
         {
-            PythonFunctions::findGame(stolen_api_key, std::stol(id), db_path);;
+            PythonFunctions::findGame(stolen_api_key, std::stol(id), db_path);
 
             Database::Statement query;
             Database::Row row;
 
-            //Check founded variants count
-            query = base.query("SELECT COUNT (`id`) FROM scraper_cache_games WHERE `game_id` = ?;");
-            query.bind(1, std::stol(id));
-            query.execute();
-            row = query.fetchRow();
-            if (row.column<size_t>(0) != 1)
+            // Check founded variants count
+            query = base.query("SELECT COUNT(`id`) FROM scraper_cache_games WHERE `game_id` = ?;");
+            query.bindMany(std::stol(id));
+            if (query.getOne<size_t>() != 1)
             {
-                //TODO: Game selecting dialog
+                // TODO: Game selecting dialog
                 break;
             }
 
-            //Read data from scraper_cache
+            // Read data from scraper_cache
             query = base.query("SELECT `name` FROM scraper_cache_games WHERE `game_id` = ?;");
-            query.bind(1, std::stol(id));
-            query.execute();
-            row = query.fetchRow();
+            query.bindMany(std::stol(id));
+            std::string name = query.getOne<std::string>();
 
-            std::string name = row.column<std::string>("name");
-
-            //Write data to games
+            // Write data to games
             query = base.query("UPDATE `games` SET `name` = ? WHERE `id` = ?;");
-            query.bind(1, name.c_str());
-            query.bind(2, std::stol(id));
+            query.bindMany(name.c_str(), std::stol(id));
             query.execute();
         }
     }
