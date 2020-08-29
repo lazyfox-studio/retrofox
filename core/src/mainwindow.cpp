83fd20cdbc73b06efa142fe331eb9404b2d3da91
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-08-29 13:45:26 +0300

    Horizontal stacking

--- core/src/mainwindow.cpp
+++ core/src/mainwindow.cpp
@@ -13,8 +13,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::initialize()
 {
-    auto temp = std::make_shared<GameMenu>(5, 5, 400, 700, 80);
-    temp->showGames(renderer, "testbase.db");
-    temp->reset();
-    root_widget = temp;
+    //auto temp = std::make_shared<GameMenu>(5, 5, 400, 700, 80);
+    //temp->showGames(renderer, "testbase.db");
+    //temp->reset();
+    //root_widget = temp;
+    auto layout = std::make_shared<Interface::Layout>(5, 5, 500, 500);
+    layout->stacking = Interface::Layout::Stacking::Horizontal;
+    auto button1 = std::make_shared<Interface::Button>(renderer, "../../core/data/testsprite.bmp", "../../core/data/testsprite.bmp");
+    auto button2 = std::make_shared<Interface::Button>(renderer, "../../core/data/testsprite.bmp", "../../core/data/testsprite.bmp");
+    layout->pushBack(button1);
+    layout->pushBack(button2);
+    root_widget = layout;
 }
