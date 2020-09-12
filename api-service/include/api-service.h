ef9d4185312f34ba3bf012d52dc5262ecbfd6b15
vla5924 <vla2000@inbox.ru>
2020-09-12 11:26:56 +0300

    added slots undef in api-service.h

--- api-service/include/api-service.h
+++ api-service/include/api-service.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#ifdef slots
+#undef slots
+#endif
+
 #include "pythonenv.h"
 #include "pythonref.h"
 #include "pythonmainthread.h"
 #include "pythonthread.h"
 #include "pythonthreadcontroller.h"
 #include "pythonfunctions.h"
+
+#ifndef slots
+#define slots
+#endif
