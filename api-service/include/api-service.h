7ba43458233d8bec90940a6550471084f12df7cc
vla5924 <vla2000@inbox.ru>
2020-09-12 10:54:03 +0300

    api-service.h header for module

---
+++ api-service/include/api-service.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pythonenv.h"
+#include "pythonref.h"
+#include "pythonmainthread.h"
+#include "pythonthread.h"
+#include "pythonthreadcontroller.h"
+#include "pythonfunctions.h"
