ea0b1d465eaf35bd49a855e4280bcd261100ae60
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2021-01-18 23:27:50 +0300

    improved execution

--- app/src/executor.cpp
+++ app/src/executor.cpp
@@ -2,5 +2,19 @@
 
 void Executor::start(QString command)
 {
-    std::system(command.toStdString().c_str());
+    if (process.state() != QProcess::ProcessState::NotRunning)
+    {
+        //TODO: do something!
+        return;
+    }
+    process.setProgram(command);
+    connect(&process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this, &Executor::recover);
+    process.start();
+    QThread::currentThread()->setPriority(QThread::Priority::IdlePriority);
+
+}
+
+void Executor::recover()
+{
+    QThread::currentThread()->setPriority(QThread::Priority::NormalPriority);
 }
--- include/app/executor.h
+++ include/app/executor.h
@@ -2,12 +2,19 @@
 
 #include <QObject>
 
-#include <cstdlib>
+#include <QProcess>
+#include <QThread>
 
 class Executor : public QObject
 {
     Q_OBJECT
 
+protected:
+    QProcess process;
+
 public:
-    Q_INVOKABLE static void start(QString command);
+    Q_INVOKABLE void start(QString command);
+
+public slots:
+    static void recover();
 };
